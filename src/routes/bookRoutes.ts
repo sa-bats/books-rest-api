40570e330598e8bef18fee596d8ad34539e13e22
sa-bats <samuil.batsinski@gmail.com>
2026-03-27 22:42:40 +0200

    import controllers and created 5 routes

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import * as bookController from "../controllers/bookController";
 
 const router = Router();
 
-// тестовый маршрут
-router.get("/", (req, res) => {
-  res.json({ message: "Books route works" });
-});
+router.get("/", bookController.getAllBooks);
+router.get("/:id", bookController.getBookById);
+router.post("/", bookController.createBook);
+router.put("/:id", bookController.updateBook);
+router.delete("/:id", bookController.deleteBook);
 
 export default router;
