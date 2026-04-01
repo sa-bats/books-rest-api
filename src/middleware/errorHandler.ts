c2576c6591e138edf4b1004fceb5c02df74c90d4
sa-bats <samuil.batsinski@gmail.com>
2026-04-01 16:19:15 +0300

    created errorHandler

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../utils/AppError";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    error: "Internal Server Error",
+  });
+};
